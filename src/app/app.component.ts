ee4680d981f5ce0a828b47d57210afe394c041a0
Dorde Masic <dorde@masic.gmail.com>
2021-11-02 14:10:02 +0100

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,4 @@
-import { Component, ViewChild, ɵɵsetComponentScope } from '@angular/core';
-import { ConstantPool, Target } from '@angular/compiler';
-import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
-
-
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -11,32 +7,62 @@ import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 })
 
 export class AppComponent {
-    access_key:string = "d8c448f5602070b64312e75c1dddcff9"
-    Http = new XMLHttpRequest;
-    phoneNumber:string = '';
-    countryCode:string = '46'; 
-    formatError:boolean = true;
+   
+    private access_key:string = "fe0f7d1b1e08cdfb4650f3342775d679";
+    private countryCode:string = '+46'; 
+   
+    phoneNo:string = '';
+    error:boolean = false;
+    warning:boolean = false;
+    valid:boolean = false; 
+    invalid:boolean = false;
     output:string = '';
     location:string = '';
     carrier:string = '';
-    error:boolean = false;
-    success:boolean = false;
-    invalid:boolean = false;
-  title: any;
+    
+    countryChange(c: any) {
+      this.countryCode = '+' + c.dialCode;
+    }
+
+    toggleError() {
+      this.error = false;
+    }
+
+    reset() {
+      this.valid = false;
+      this.invalid = false;
+      this.error = false;
+      this.warning = false;
+      this.location = '';
+      this.carrier = '';
+      this.output = '';
+    }
+
+    checkCountryCode(no: string) {
+      if (no.charAt(0) == "+") {
+        this.countryCode = '';
+      }
+    }
+
+    enterSubmit(e: any) {
+     
+      if (e.keyCode == 13) {
+        this.phoneNo.replace(/\n/g, '')
+        this.getInfo()
+      }
+    }
 
     parseData(data: any) {
 
-      this.output = '+' + this.countryCode + ' ' + this.phoneNumber
+      this.output = this.countryCode + ' ' + this.phoneNo;
       
       if (data.valid) {
-  
         let carrier = data.carrier;
-        let country = data.country_name;
         let location = data.location;
         
-        this.success = true;
+        this.valid = true;
         this.output += ' is valid'
-        this.location = 'Location: ' + country;
+        this.location = 'Location: ' + data.country_name;
 
         if (location != '') {
           this.location += ', ' + location;
@@ -46,73 +72,43 @@ export class AppComponent {
           this.carrier = 'Carrier: ' + data.carrier;
         } 
 
-      } else {
+      } 
+      
+      if (data.valid == false) {
+        
         this.invalid = true;
         this.output += ' is invalid'
       }
-    }
 
-    reset() {
-      this.success = false;
-      this.invalid = false;
-      this.error = false;
-      this.location = '';
-      this.carrier = '';
-      this.output = '';
-
-    }
-
-    countryChange(c: any) {
-      this.countryCode = c.dialCode;
-    }
-
-    hasError(e: boolean) {
-      console.log(e)
-      this.formatError = e;
-    }
-
-    checkCountryCode(no: string) {
-      // checks if user provided country prefix
-      if (no.charAt(0) == "+") {
-        this.countryCode = '';
-      }
-    }
-
-    toggleError() {
-      this.error = false;
-    }
+      if (data.success == false) {
+       
+        this.warning = true;
+        this.output = data.error.info;
+      } 
+}
     
-    getInfo(phoneNo: string) {
+
+    getInfo() {
       
       this.reset(); 
-      if (phoneNo != '') {
-        if (this.formatError) {
-          this.checkCountryCode(phoneNo)
-          let url="http://apilayer.net/api/validate?access_key=" + this.access_key + "&number=" + this.countryCode + phoneNo;
-
-          fetch(url)
+      
+      if (this.phoneNo != '') {
+        
+          this.checkCountryCode(this.phoneNo)
+          fetch("http://apilayer.net/api/validate?access_key=" + this.access_key + "&number=" + this.countryCode + this.phoneNo)
           .then(response => response.json())
           .then((data) => {
-
-            this.phoneNumber = phoneNo;
+           
             this.parseData(data);
           
           }).catch((error) => {
             console.log(error);
           });
-        } else {
-          this.output = 'Phone number has the wrong format!';
-          this.invalid = true;
-          this.error = true;
-          
-        }
-      
+    
       } else {
         this.output = 'Please provide a phone number';
-        this.invalid = true;
         this.error = true;
+        this.warning = true;
       }
-
     } 
-     
 }
