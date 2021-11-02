726b0e418102702ba71f89401eb05f5c5cb60df0
Dorde Masic <dorde@masic.gmail.com>
2021-11-02 15:49:44 +0100

    fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,6 @@ import { Component } from '@angular/core';
 })
 
 export class AppComponent {
-   
     private access_key:string = "fe0f7d1b1e08cdfb4650f3342775d679";
     private countryCode:string = '+46'; 
    
@@ -47,8 +46,8 @@ export class AppComponent {
     enterSubmit(e: any) {
      
       if (e.keyCode == 13) {
-        this.phoneNo.replace(/\n/g, '')
-        this.getInfo()
+        this.phoneNo.replace(/\n/g, '');
+        this.getInfo();
       }
     }
 
@@ -71,44 +70,40 @@ export class AppComponent {
         if (carrier != '') {
           this.carrier = 'Carrier: ' + data.carrier;
         } 
-
       } 
       
       if (data.valid == false) {
-        
         this.invalid = true;
         this.output += ' is invalid'
       }
 
       if (data.success == false) {
-       
         this.warning = true;
         this.output = data.error.info;
       } 
 }
     
-
     getInfo() {
-      
-      this.reset(); 
-      
-      if (this.phoneNo != '') {
-        
-          this.checkCountryCode(this.phoneNo)
-          fetch("http://apilayer.net/api/validate?access_key=" + this.access_key + "&number=" + this.countryCode + this.phoneNo)
-          .then(response => response.json())
-          .then((data) => {
-           
-            this.parseData(data);
+
+        this.reset(); 
+
+        if (this.phoneNo != '' && (/^\d+$/.test(this.phoneNo))) {
+
+            this.checkCountryCode(this.phoneNo)
+            fetch("http://apilayer.net/api/validate?access_key=" + this.access_key + "&number=" + this.countryCode + this.phoneNo)
+            .then(response => response.json())
+            .then((data) => {
+            
+              this.parseData(data);
+            
+            }).catch((error) => {
+              console.log(error);
+            });
           
-          }).catch((error) => {
-            console.log(error);
-          });
-    
-      } else {
-        this.output = 'Please provide a phone number';
-        this.error = true;
-        this.warning = true;
-      }
+        } else {
+          this.output = 'Please provide a phone number';
+          this.error = true;
+          this.warning = true;
+        }
     } 
 }
